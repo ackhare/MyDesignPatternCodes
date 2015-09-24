1be14d1b588f8e96a417bf60ed543cd6a1740840
chetan <bairagisirfire@gmail.com>
2015-09-24 12:28:29 +0530

    My first commit

---
+++ src/AbstractFactory/AbstractFactoryWithExplanation/AbstractWidgetFactory.java
@@ -0,0 +1,6 @@
+package AbstractFactory.AbstractFactoryWithExplanation;
+
+//AbstractFactory
+public interface AbstractWidgetFactory{
+    public Window createWindow();
+}
---
+++ src/AbstractFactory/AbstractFactoryWithExplanation/MacOSXWidgetFactory.java
@@ -0,0 +1,11 @@
+package AbstractFactory.AbstractFactoryWithExplanation;
+
+//And for MacOSX:
+    //ConcreteFactory2
+    public class MacOSXWidgetFactory{
+        //create a MacOSXWindow
+        public Window createWindow(){
+            MacOSXWindow window = new MacOSXWindow();
+            return window;
+        }
+    }
