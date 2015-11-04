9a7311317e6aa64ad9208a7f37a28803cc9a6d32
chetan <bairagisirfire@gmail.com>
2015-11-04 22:36:33 +0530

    till factory

---
+++ src/FactoryPatternHFS/pizzaaf/ConcreteProduct/ClamPizza.java
@@ -0,0 +1,20 @@
+package FactoryPatternHFS.pizzaaf.ConcreteProduct;
+
+import FactoryPatternHFS.pizzaaf.Product.Pizza;
+import FactoryPatternHFS.pizzaaf.Factory.PizzaIngredientFactory;
+
+public class ClamPizza extends Pizza {
+	PizzaIngredientFactory ingredientFactory;
+ 
+	public ClamPizza(PizzaIngredientFactory ingredientFactory) {
+		this.ingredientFactory = ingredientFactory;
+	}
+ 
+	public void prepare() {
+		System.out.println("Preparing " + name);
+		dough = ingredientFactory.createDough();
+		sauce = ingredientFactory.createSauce();
+		cheese = ingredientFactory.createCheese();
+		clam = ingredientFactory.createClam();
+	}
+}
